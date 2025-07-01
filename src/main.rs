f925ae89bf5351317847cef6600c49c8393d3f59
Ravikiran kalal <ravikiran.kalal@flipkart.com>
2025-07-01 11:12:28 +0000

    skdfljk;lsf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+
+use axum::{routing::get, routing::Router, Json};
+use axum::extract::Path;
+use serde_json::{json, Value};
+use std::net::SocketAddr;
+
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health", get(health))
+        .route("/echo/{msg}", get(echo));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on http://{}", addr);
+    axum::serve(
+        tokio::net::TcpListener::bind(addr).await.unwrap(),
+        app.into_make_service()
+    )
+    .await
+    .unwrap();
+}
+
+
+async fn root() -> &'static str {
+    "Hello, world!"
+}
+
+async fn health() -> Json<Value> {
+    Json(json!({ "status": "ok" }))
+}
+
+async fn echo(Path(msg): Path<String>) -> Json<Value> {
+    Json(json!({ "echo": msg }))
 }
